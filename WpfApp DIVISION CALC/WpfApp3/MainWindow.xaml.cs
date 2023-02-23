2365c417f29f3b5fa12b591a86c6b2740e221e35
Sergio Contreras Palomera <112669671+zlxscp@users.noreply.github.com>
2023-02-22 20:27:23 -0600

    Add files via upload

---
+++ WpfApp DIVISION CALC/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            double v1 = double.Parse(textBox.Text);
+            double v2 = double.Parse(textBox2.Text);
+            textBox3.Text = (v1 / v2).ToString();
+        }
+    }
+}
