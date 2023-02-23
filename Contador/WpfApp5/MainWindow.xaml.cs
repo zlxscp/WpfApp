2305653f8cfd4426c9d6e34d5200303eb8a00ba4
Sergio Contreras Palomera <112669671+zlxscp@users.noreply.github.com>
2023-02-22 20:22:34 -0600

    Add files via upload

---
+++ Contador/WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int[] arreglo = new int[3];
+        byte contador = 0;
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int valor= int.Parse(txt1.Text);
+            arreglo[contador] = valor;
+            contador++;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            for(int i=0; i<arreglo.Length; i++)
+            {
+                listBox.Items.Add(arreglo[i]);
+            }
+        }
+    }
+}
